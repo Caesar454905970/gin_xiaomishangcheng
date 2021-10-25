ed5bad5d92b069605a1b4813244b02ead92af7ff
Caesar454905970 <454905970@qq.com>
2021-10-25 19:33:28 +0800

    GIN文件按时间分类上传

--- 11_gin_demo11/main.go
+++ 11_gin_demo11/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"11_gin_demo11/models"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
@@ -21,24 +20,28 @@ func main() {
 		username := c.PostForm("username")
 		//1、获取上传的文件
 		file, err := c.FormFile("face")
-		if err == nil {
-			//2、获取文件的后缀名 判断类型是否正确 .jpg .png .gif .jpeg
-			extName := path.Ext(file.Filename)
-
-			//允许上传的格式
-			allowExtMap := map[string]bool{
-				".jpg":  true,
-				".png":  true,
-				".gif":  true,
-				".jpeg": true,
-			}
-			if _, ok := allowExtMap[extName]; !ok {
-				c.JSON(200, gin.H{
-					"msg": "上传的文件不合法",
-				})
-				//退出
-				return
-			}
+		if err != nil {
+			//失败：获取上传的文件
+			c.JSON(200, gin.H{
+				"msg": "失败：获取上传的文件",
+			})
+		}
+		//成功：获取上传的文件
+		//2、获取文件的后缀名 判断类型是否正确 .jpg .png .gif .jpeg
+		extName := path.Ext(file.Filename)
+		//允许上传的格式
+		allowExtMap := map[string]bool{
+			".jpg":  true,
+			".png":  true,
+			".gif":  true,
+			".jpeg": true,
+		}
+		if _, ok := allowExtMap[extName]; !ok {
+			c.JSON(200, gin.H{
+				"msg": "上传的文件不合法",
+			})
+			//退出
+			return
 		}
 		//3、创建图片保存的目录  static /upload/2021/1021
 		//	获取今天的日期
@@ -51,16 +54,13 @@ func main() {
 			})
 		}
 		//生产文件名称和文件保存的目录
-		//文件名 = 获取时间戳 +上传接收到的文件名称
-		fileName := strconv.FormatInt(models.GetUnix(), 10) + extName
-		fmt.Printlin(extName)
+		//文件名 = 获取时间戳--上传接收到的文件名称
+		fileName := strconv.FormatInt(models.GetUnix(), 10) + "--" + extName
 		//5、执行上传
-		dst := path.Join("./static/upload", file.Filename)
-		if err == nil {
-			//成功
-			//存储文件到本地
-			c.SaveUploadedFile(file, dst)
-		}
+		dst := path.Join("./static/upload", fileName)
+		//成功
+		//存储文件到本地
+		c.SaveUploadedFile(file, dst)
 		c.JSON(200, gin.H{
 			"msg": "上传成功",
 		})
