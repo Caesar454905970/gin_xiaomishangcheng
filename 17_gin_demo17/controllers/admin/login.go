5f20f8d7e1634a7ffede4a7d1766e9ec5fd85ebb
Caesar454905970 <454905970@qq.com>
2021-11-01 17:18:08 +0800

    使用base64Captcha生成图形验证码(使用redis)-新增一个前端判断字段

--- 17_gin_demo17/controllers/admin/login.go
+++ 17_gin_demo17/controllers/admin/login.go
@@ -22,17 +22,26 @@ func (con LoginController) DoLogin(c *gin.Context) {
 	verifyValue := c.PostForm("verifyValue")
 	//排除前端提交的空请求
 	if captchId == "" || verifyValue == "" {
-		c.JSON(http.StatusOK, "验证码验证失败")
+		c.JSON(http.StatusOK, gin.H{
+			"msg":               "验证码验证失败",
+			"verifyValueResult": false,
+		})
 		return
 	}
 	//调用验证验证码的方法
 	if flag := models.VerifyCaptcha(captchId, verifyValue); flag == true {
 		//验证通过
 		fmt.Println(flag)
-		c.JSON(http.StatusOK, "验证码验证成功")
+		c.JSON(http.StatusOK, gin.H{
+			"msg":               "验证码验证成功",
+			"verifyValueResult": true,
+		})
 	} else {
 		//验证失败
-		c.JSON(http.StatusOK, "验证码验证失败")
+		c.JSON(http.StatusOK, gin.H{
+			"msg":               "验证码验证失败",
+			"verifyValueResult": false,
+		})
 	}
 
 }
